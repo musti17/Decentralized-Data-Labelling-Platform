0fd0f6e43f35399333dcc636d1a101468b8b19d0
Mustafa Zahid <mustafazahid167@gmail.com>
2024-07-21 22:30:12 +0500

    Database schema designed using Prisma ORM Database migrated using prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,3 @@
 import express from "express";
 const app = express();
 
-// postgresql://DataLabellingPlatform_owner:dyW9ntSb6aXo@ep-twilight-sea-a579xpfq.us-east-2.aws.neon.tech/DataLabellingPlatform?sslmode=require
